99184578da926f502b9b97cfdc9bc8d655b6b43c
Amit Vikram <amittengradepoint@gmail.com>
2018-01-09 21:31:23 +0530

    week2 started

---
+++ del.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	int temp = 10;
+	int *p = &temp	;
+	int temp1 = 20;
+	int **q = &p;
+}
---
+++ week2/mergeSort.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<limits.h>
+#include<bool.h>
+
+void merge(FILE *fp, int l, int r, int mid){
+	
+
+int main(){
+	char filename[32];
+	printf("Enter FileName: ");
+	scanf("%s", FileName);
+	fp = fopen(FileName, 'r');
+	if(fp == NULL){
+		printf("Error in Opening File");
+		return 0;
+	}
+	
+	
